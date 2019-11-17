a1c36155f6101564a6d7996da1b19d9a8fd66b75
Andrew Ty Nguyen <andyt.nguyen123@gmail.com>
2019-11-17 01:32:32 -0700

    navigating flows

--- tracks/src/screens/SignupScreen.js
+++ tracks/src/screens/SignupScreen.js
@@ -1,8 +1,20 @@
 import React from 'react';
-import { View, Text, StyleSheet} from 'react-native';
+import { View, Text, StyleSheet, Button } from 'react-native';
 
-const SignupScreen = () => {
-  return <Text style={{ fontSize: 48 }}>Sign up Screen</Text>
+const SignupScreen = ({navigation}) => {
+  return (
+    <>
+    <Text style={{ fontSize: 48 }}>Sign up Screen</Text>
+    <Button
+      title="Go to Signin"
+      onPress={() => navigation.navigate('Signin')}
+    />
+    <Button
+      title="Go to Main Flow"
+      onPress={() => navigation.navigate('mainFlow')}
+    />
+    </>
+  )
 }
 
 const styles = StyleSheet.create({});
